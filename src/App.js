134039e7a0d6ffc05a87f30631f4ba60e2d0d7f3
Ayu-coder530 <a.das.9473@westcliff.edu>
2024-08-10 21:15:48 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Animation1 from './animation1/App';
+import Animation2 from './animation2/App';
+import Animation3 from './animation3/App';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/animation1">Animation 1</Link>
+            </li>
+            <li>
+              <Link to="/animation2">Animation 2</Link>
+            </li>
+            <li>
+              <Link to="/animation3">Animation 3</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/animation1" component={Animation1} />
+          <Route path="/animation2" component={Animation2} />
+          <Route path="/animation3" component={Animation3} />
+          <Route path="/" exact component={() => <div>Welcome to the Animation App</div>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { render } from "react-dom";
-import Tree from "./Tree";
-import data from "./data";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const App = () => {
-  return <Tree data={data} width={600} height={500} />;
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-render(<App />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
